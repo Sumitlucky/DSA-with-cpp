04f883769ab8f3d4de9d5d8c1b0fa1b3379de7dd
Sumit Lucky <you@example.com>
2025-10-06 23:37:19 +0530

    updated code

---
+++ pattern.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//   int n =  5;
+//   for(int i = 0; i<n;i++){
+//     for(int j=0; i<n;i++) {
+//       cout <<"* ";
+//     }
+//     cout << endl;
+//   }
+// }
+// int main() {
+//   int n =  5;
+//   for(int i = 0; i<n;i++){
+//     for(int j = 0 ; j<=i;j++){
+//       cout<<"* ";
+//     }
+//     cout<< endl;
+//    }
+// }
+
+// int main() {
+//   int n =  5;
+//   for(int i = 1; i<=n;i++){
+//     for(int j = 1 ; j<=i;j++){
+//       cout<<j;
+//     }
+//     cout<< endl;
+//    }
+// }
+
+// int main() {
+//   int n =  5;
+//   for(int i = 0; i<n;i++){
+//     for(int j = i ; j<n;j++){
+//       cout<<"*";
+      
+//     }
+//     cout<< endl;
+//    }
+// }
+
+
+// int main() {
+//   int n =  5;
+//   for(int i = 0; i<n;i++){
+//     for(int j = 1 ; j<=n-i;j++){
+//       cout<<j;
+//     }
+//     cout<< endl;
+//    }
+// }
+
+
+
+// int main() {
+//   int n =  5;
+//   for(int i = 1; i<=n;i++){
+//     for(int j = 1 ; j<=n-i;j++){
+//       cout<<" ";
+//     }
+//     for(int j = 1;j<=2*i-1;j++) {
+//       cout<<"*";
+//     }
+//     cout<< endl;
+//   }
+// }  
+
+
+// int main() {
+//   int n =  5;
+//   for(int i = 1; i<=n;i++){
+//     for(int j = 1 ; j<i;j++){
+//       cout<<" ";
+//     }
+//     for(int j = 1;j<=2*n-2*i+1;j++) {
+//       cout<<"*";
+//     }
+//     cout<< endl;
+//   }  
+// }
+
+
+int main() {
+  int n =  6;
+  for(int i = 1; i<=n;i++){
+    for(int j = 1 ; j<=n-i;j++){
+      cout<<" ";
+    }
+    for(int j = 1;j<=2*i-1;j++) {
+      cout<<"*";
+    }
+    cout<< endl;
+  }
+  for(int i = 1; i<=n;i++){
+    for(int j = 1 ; j<i;j++){
+      cout<<" ";
+    }
+    for(int j = 1;j<=2*n-2*i+1;j++) {
+      cout<<"*";
+    }
+    cout<< endl;
+  }  
+}
